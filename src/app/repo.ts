a04f7b26a0778a934cf6ce1e190a8a45718f3095
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-18 08:11:00 +0300

    it will add object to class repo

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,2 +1,4 @@
 export class Repo {
+    constructor(public name:string, public html_url:string, public fork_url:number, public watching:number, public languages:string,public description:string, public created_at:Date){
+    }
 }
