67b1a45998886966c7ba7ee2454aa19c7ef0a763
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-18 12:34:18 +0300

    it will generate the date count pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { SearchFormComponent } from './search-form/search-form.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
