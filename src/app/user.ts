d320ec90e7de4457807831d70542b34794f1be5a
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-18 08:09:17 +0300

    it will add object to the user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,4 @@
 export class User {
-}
+    constructor( public name:string, public login:string, public html_url:string, public location:string, public following:number, public followers:number, public public_repos:number, public avatar_url:string, public created_at:Date){
+    }
+    }
